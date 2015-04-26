aedbbba17cadf3e4c6c2b881b206eced220930c0
Aaron <amcintyre2@mail.csuchico.edu>
2015-04-26 15:29:21 -0700

    Connecting and controlling drone

--- DroneBrain/app/src/main/java/com/doubleacoding/dronebrain/DroneBrainService.java
+++ DroneBrain/app/src/main/java/com/doubleacoding/dronebrain/DroneBrainService.java
@@ -8,7 +8,6 @@ import android.location.LocationProvider;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.util.Log;
 
 import com.doubleacoding.dronebrain.lib.KalmanLocationManager;
 import com.github.nkzawa.emitter.Emitter;
@@ -17,13 +16,7 @@ import com.github.nkzawa.socketio.client.Socket;
 
 import org.json.JSONObject;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-
 import java.net.URISyntaxException;
-import java.net.UnknownHostException;
 
 /**
  * Created by Aaron on 4/25/2015.
@@ -49,40 +42,42 @@ public class DroneBrainService extends Service {
         public void onLocationChanged(Location location) {
             if (mSocket.connected()) {
                 JSONObject message = LocationJSONHelper.createJSONLocation(location.getLatitude(), location.getLongitude());
+
+                //this line sends the location as a JSONObject as the event 'location'
                 mSocket.emit("location", message);
             }
         }
 
-            @Override
-            public void onStatusChanged (String provider,int status, Bundle extras){
-                String statusString = "Unknown";
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            String statusString = "Unknown";
 
-                switch (status) {
+            switch (status) {
 
-                    case LocationProvider.OUT_OF_SERVICE:
-                        statusString = "Out of service";
-                        break;
+                case LocationProvider.OUT_OF_SERVICE:
+                    statusString = "Out of service";
+                    break;
 
-                    case LocationProvider.TEMPORARILY_UNAVAILABLE:
-                        statusString = "Temporary unavailable";
-                        break;
+                case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                    statusString = "Temporary unavailable";
+                    break;
 
-                    case LocationProvider.AVAILABLE:
-                        statusString = "Available";
-                        break;
-                }
+                case LocationProvider.AVAILABLE:
+                    statusString = "Available";
+                    break;
             }
+        }
 
-            @Override
-            public void onProviderEnabled (String provider){
+        @Override
+        public void onProviderEnabled(String provider) {
 
-            }
+        }
 
-            @Override
-            public void onProviderDisabled (String provider){
+        @Override
+        public void onProviderDisabled(String provider) {
 
-            }
-        };
+        }
+    };
 
     @Override
     public void onDestroy() {
@@ -91,73 +86,76 @@ public class DroneBrainService extends Service {
     }
 
     @Override
-        public IBinder onBind(Intent intent) {
-            return droneBind;
-        }
+    public IBinder onBind(Intent intent) {
+        return droneBind;
+    }
 
-        @Override
-        public boolean onUnbind(Intent intent) {
-            return false;
-        }
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return false;
+    }
 
-        @Override
-        public void onCreate() {
-            super.onCreate();
-            mKalmanLocationManager = new KalmanLocationManager(this);
-            try{
-                mSocket = IO.socket("http://" + mServerIpAddress + ":" + mServerPort);
-            } catch (URISyntaxException e) {
-                e.printStackTrace();
-            }
-        }
-
-        @Override
-        public int onStartCommand(Intent intent, int flags, int startId) {
-            mKalmanLocationManager.requestLocationUpdates(KalmanLocationManager.UseProvider.GPS_AND_NET, FILTER_TIME, GPS_TIME, NET_TIME, mLocationListener, true);
-            mIsRequestingUpdates = true;
-            mSocket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
-
-                @Override
-                public void call(Object... args) {
-                    mSocketConnected = true;
-                }
-
-            }).on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
-
-                @Override
-                public void call(Object... args) {}
-
-            });
-            mSocket.connect();
-            return Service.START_NOT_STICKY;
-        }
-
-        public void takeoff() {
-            if (mSocket.connected()) {
-                mSocket.emit("takeoff", "TAKEOFFDRONE!!!");
-            }
-        }
-
-        public void calibrate() {
-            if (mSocket.connected()) {
-                mSocket.emit("calibrate", "CALIBRATEDRONE!!!");
-            }
-        }
-
-        public void reset() {
-            if (mSocket.connected()) {
-                mSocket.emit("reset", "RESETDRONE!!!");
-            }
-        }
-
-        public void stop() {
-            mKalmanLocationManager.removeUpdates(mLocationListener);
-            stopSelf();
-        }
-
-        public class DroneBinder extends Binder {
-            DroneBrainService getService() {
-                return DroneBrainService.this;
-            }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mKalmanLocationManager = new KalmanLocationManager(this);
+        try {
+            mSocket = IO.socket("http://" + mServerIpAddress + ":" + mServerPort);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
         }
     }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mKalmanLocationManager.requestLocationUpdates(KalmanLocationManager.UseProvider.GPS_AND_NET, FILTER_TIME, GPS_TIME, NET_TIME, mLocationListener, true);
+        mIsRequestingUpdates = true;
+        mSocket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+
+            @Override
+            public void call(Object... args) {
+                mSocketConnected = true;
+            }
+
+        }).on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
+
+            @Override
+            public void call(Object... args) {
+                mSocketConnected = false;
+            }
+
+        });
+        mSocket.connect();
+        return Service.START_NOT_STICKY;
+    }
+    //emit the event takeoff on the socket to the server.
+    public void takeoff() {
+        if (mSocket.connected()) {
+            mSocket.emit("takeoff", "TAKEOFFDRONE!!!");
+        }
+    }
+    //emit the event calibrate on the socket to the server
+    public void calibrate() {
+        if (mSocket.connected()) {
+            mSocket.emit("calibrate", "CALIBRATEDRONE!!!");
+        }
+    }
+
+    //emit the event reset on the socket connection to the server
+    public void reset() {
+        if (mSocket.connected()) {
+            mSocket.emit("testdata", "testdata!!!");
+        }
+    }
+    //stop location updates and end the service.
+    public void stop() {
+        mKalmanLocationManager.removeUpdates(mLocationListener);
+        stopSelf();
+    }
+
+    public class DroneBinder extends Binder {
+        DroneBrainService getService() {
+            return DroneBrainService.this;
+        }
+    }
+}
