fa50b530ec351393ec645d408ef29f4ad19cc6bf
Aaron <amcintyre2@mail.csuchico.edu>
2015-05-01 01:17:38 -0700

    Add Buttons and fix layouts
    Implemented Butterknife to clean up some of the broilerplate code I was
    using. Added a toggle switch to change between location manager and kalman
    location manager. Added location manager.

--- DroneBrain/app/src/main/java/com/doubleacoding/dronebrain/DroneBrainService.java
+++ DroneBrain/app/src/main/java/com/doubleacoding/dronebrain/DroneBrainService.java
@@ -1,13 +1,16 @@
 package com.doubleacoding.dronebrain;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.location.LocationProvider;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.util.Log;
 
 import com.doubleacoding.dronebrain.lib.KalmanLocationManager;
 import com.github.nkzawa.emitter.Emitter;
@@ -29,6 +32,7 @@ public class DroneBrainService extends Service {
     private static final long GPS_TIME = 1000;
     private static final long NET_TIME = 5000;
     private static final long FILTER_TIME = 200;
+    private static final int MINACCURACY = 8;
 
     private KalmanLocationManager mKalmanLocationManager;
     private boolean mIsRequestingUpdates = false;
@@ -36,13 +40,48 @@ public class DroneBrainService extends Service {
     private String mServerIpAddress = "192.168.0.1";
     private int mServerPort = 4242;
     private boolean mSocketConnected;
+    private NavDataUpdate navDataUpdate;
+    boolean mUsingGPSKalmanFilter;
+    private LocationManager mLocationManager;
+
+    public interface NavDataUpdate {
+        public void updateNavData(JSONObject navData);
+        public void updateLocalPos(JSONObject localprose);
+    }
+
+    //this is used to set the nave data update interface
+    public void setNavDataInterface(NavDataUpdate updateInterface){
+        navDataUpdate = updateInterface;
+    }
+
+    public void toggleUsingKalmanFilter() {
+        if(mIsRequestingUpdates){
+            if(mKalmanLocationManager != null) {
+                Log.i(TAG, "Using Location Manager");
+                mKalmanLocationManager.removeUpdates(mLocationListener);
+                mKalmanLocationManager = null;
+                mUsingGPSKalmanFilter = false;
+                getLocationManager();
+            } else if(mLocationManager != null) {
+                Log.i(TAG, "Using Kalman Location Manager");
+                mLocationManager.removeUpdates(mLocationListener);
+                mLocationManager = null;
+                mUsingGPSKalmanFilter = true;
+                getKalmanLocationManager();
+            }
+        }
+    }
+
+    public boolean isUsingKalmanFilter() {
+        return mUsingGPSKalmanFilter;
+    }
 
     private LocationListener mLocationListener = new LocationListener() {
         @Override
         public void onLocationChanged(Location location) {
-            if (mSocket.connected()) {
+            if (mSocket.connected() && location.getAccuracy() < MINACCURACY) {
                 JSONObject message = LocationJSONHelper.createJSONLocation(location.getLatitude(), location.getLongitude());
-
+                Log.i(TAG, "Accuracy " + location.getAccuracy());
                 //this line sends the location as a JSONObject as the event 'location'
                 mSocket.emit("location", message);
             }
@@ -79,9 +118,11 @@ public class DroneBrainService extends Service {
         }
     };
 
+
     @Override
     public void onDestroy() {
         super.onDestroy();
+        stop();
         mSocket.disconnect();
     }
 
@@ -98,7 +139,7 @@ public class DroneBrainService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        mKalmanLocationManager = new KalmanLocationManager(this);
+        mIsRequestingUpdates = false;
         try {
             mSocket = IO.socket("http://" + mServerIpAddress + ":" + mServerPort);
         } catch (URISyntaxException e) {
@@ -108,8 +149,40 @@ public class DroneBrainService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        mKalmanLocationManager.requestLocationUpdates(KalmanLocationManager.UseProvider.GPS_AND_NET, FILTER_TIME, GPS_TIME, NET_TIME, mLocationListener, true);
+        startLocationUpdates();
+        setupSocketConnection();
+        return Service.START_NOT_STICKY;
+    }
+
+    private void startLocationUpdates() {
+        if(mUsingGPSKalmanFilter) {
+            getKalmanLocationManager();
+        } else {
+            getLocationManager();
+        }
         mIsRequestingUpdates = true;
+    }
+
+    private void getLocationManager() {
+        if(mLocationManager == null) {
+            mLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        }
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mLocationListener);
+    }
+
+    private void getKalmanLocationManager() {
+        if(mKalmanLocationManager == null){
+            mKalmanLocationManager = new KalmanLocationManager(this);
+        }
+        mKalmanLocationManager.requestLocationUpdates(KalmanLocationManager.UseProvider.GPS_AND_NET,
+                FILTER_TIME,
+                GPS_TIME,
+                NET_TIME,
+                mLocationListener,
+                true);
+    }
+
+    private void setupSocketConnection() {
         mSocket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
 
             @Override
@@ -117,6 +190,25 @@ public class DroneBrainService extends Service {
                 mSocketConnected = true;
             }
 
+        }).on("dronedata", new Emitter.Listener(){
+
+            @Override
+            public void call(Object... args) {
+                JSONObject data = (JSONObject) args[0];
+                if(navDataUpdate != null)
+                    navDataUpdate.updateNavData(data);
+                Log.i(TAG, data.toString());
+
+            }
+        }).on("localxy", new Emitter.Listener() {
+
+            @Override
+            public void call(Object... args) {
+                JSONObject data = (JSONObject) args[0];
+                if(navDataUpdate != null) {
+                    navDataUpdate.updateLocalPos(data);
+                }
+            }
         }).on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
 
             @Override
@@ -126,7 +218,6 @@ public class DroneBrainService extends Service {
 
         });
         mSocket.connect();
-        return Service.START_NOT_STICKY;
     }
     //emit the event takeoff on the socket to the server.
     public void takeoff() {
@@ -134,6 +225,7 @@ public class DroneBrainService extends Service {
             mSocket.emit("takeoff", "TAKEOFFDRONE!!!");
         }
     }
+
     //emit the event calibrate on the socket to the server
     public void calibrate() {
         if (mSocket.connected()) {
@@ -144,14 +236,72 @@ public class DroneBrainService extends Service {
     //emit the event reset on the socket connection to the server
     public void reset() {
         if (mSocket.connected()) {
-            mSocket.emit("testdata", "testdata!!!");
+            mSocket.emit("reset", "RESET!!!");
         }
     }
+    public void forward() {
+        if (mSocket.connected()) {
+            mSocket.emit("forward", "FORWARD");
+        }
+    }
+    public void backward() {
+        if (mSocket.connected()) {
+            mSocket.emit("backward", "BACKWARD");
+        }
+    }
+    public void left() {
+        if (mSocket.connected()) {
+            mSocket.emit("left", "LEFT");
+        }
+    }
+    public void right() {
+        if (mSocket.connected()) {
+            mSocket.emit("right", "RIGHT!!!");
+        }
+    }
+    public void clockwise() {
+        if (mSocket.connected()) {
+            mSocket.emit("clockwise", "CLOCKWISE!!!");
+        }
+    }
+    public void up() {
+        if (mSocket.connected()) {
+            mSocket.emit("up", "UP!!!");
+        }
+    }
+    public void down() {
+        if (mSocket.connected()) {
+            mSocket.emit("down", "DOWN!!!");
+        }
+    }
+    public void zero() {
+        if (mSocket.connected()) {
+            mSocket.emit("zero", "ZERO!!");
+        }
+    }
+
+    public void followMe() {
+        if (mSocket.connected()) {
+            mSocket.emit("follow", "FOLLOW!!");
+        }
+    }
+
     //stop location updates and end the service.
     public void stop() {
-        mKalmanLocationManager.removeUpdates(mLocationListener);
+        if(mIsRequestingUpdates) {
+            if(mKalmanLocationManager != null){
+                mKalmanLocationManager.removeUpdates(mLocationListener);
+            } else if(mLocationManager != null) {
+                mLocationManager.removeUpdates(mLocationListener);
+            }
+            mIsRequestingUpdates = false;
+        }
+        if(mSocket.connected()) {
+            mSocket.emit("stop", "STOP!!!!");
+        }
         stopSelf();
     }
+
 
     public class DroneBinder extends Binder {
         DroneBrainService getService() {
--- DroneBrain/app/src/main/java/com/doubleacoding/dronebrain/MainActivity.java
+++ DroneBrain/app/src/main/java/com/doubleacoding/dronebrain/MainActivity.java
@@ -4,34 +4,56 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
-import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.melnykov.fab.FloatingActionButton;
 
+import org.json.JSONObject;
 
-public class MainActivity extends ActionBarActivity {
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+
+
+public class MainActivity extends ActionBarActivity implements DroneBrainService.NavDataUpdate {
+    @InjectView(R.id.flyingstatedata) TextView mFlyingState;
+    @InjectView(R.id.batterypercentagedata) TextView mBatteryPercent;
+    @InjectView(R.id.rotationdata) TextView mRotation;
+    @InjectView(R.id.velocitydata) TextView mVelocity;
+    @InjectView(R.id.magnetodata) TextView mMagneto;
+    @InjectView(R.id.gpsdata) TextView mGPS;
+    @InjectView(R.id.xpos) TextView mXPos;
+    @InjectView(R.id.ypos) TextView mYPos;
+
+    @InjectView(R.id.takeofflandlayout) LinearLayout takeoffLand;
+    @InjectView(R.id.calibratelayout) LinearLayout calibrate;
+    @InjectView(R.id.resetlayout) LinearLayout reset;
+    @InjectView(R.id.stoplayout) LinearLayout stopall;
+    @InjectView(R.id.buttonpanel) LinearLayout buttonPanel;
+    @InjectView(R.id.buttonpanel2) LinearLayout buttonPanel2;
+    @InjectView(R.id.navdatapanel) LinearLayout navDataPanel;
 
     //m-variables for service management.
     private DroneBrainService mDroneService;
     private Intent mDroneIntent;
     private boolean mDroneBound;
+    private boolean mIsUsingKalman;
 
     private ServiceConnection mServiceConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
-            DroneBrainService.DroneBinder binder = (DroneBrainService.DroneBinder)service;
+            DroneBrainService.DroneBinder binder = (DroneBrainService.DroneBinder) service;
             mDroneService = binder.getService();
+            mDroneService.setNavDataInterface(MainActivity.this);
+            mIsUsingKalman = mDroneService.isUsingKalmanFilter();
             mDroneBound = true;
         }
 
@@ -44,7 +66,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        if(mDroneIntent == null) {
+        if (mDroneIntent == null) {
             mDroneIntent = new Intent(this, DroneBrainService.class);
         }
         bindService(mDroneIntent, mServiceConnection, Context.BIND_AUTO_CREATE);
@@ -61,60 +83,109 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final LinearLayout takeoffLand = (LinearLayout) findViewById(R.id.takeofflandlayout);
-        final LinearLayout calibrate = (LinearLayout) findViewById(R.id.calibratelayout);
-        final LinearLayout reset = (LinearLayout) findViewById(R.id.resetlayout);
-        final LinearLayout stopall = (LinearLayout) findViewById(R.id.stoplayout);
-        final FloatingActionButton mainFab = (FloatingActionButton) findViewById(R.id.mainfab);
-        mainFab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(takeoffLand.getVisibility() == View.GONE){
-                    takeoffLand.setVisibility(View.VISIBLE);
-                    reset.setVisibility(View.VISIBLE);
-                    calibrate.setVisibility(View.VISIBLE);
-                    stopall.setVisibility(View.VISIBLE);
-
-                } else {
-                    takeoffLand.setVisibility(View.GONE);
-                    reset.setVisibility(View.GONE);
-                    calibrate.setVisibility(View.GONE);
-                    stopall.setVisibility(View.GONE);
-                }
-            }
-        });
-        FloatingActionButton stopButton = (FloatingActionButton) findViewById(R.id.stop);
-        stopButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(mDroneService != null) {
-                    mDroneService.stop();
-                }
-            }
-        });
-        FloatingActionButton calibrateButton = (FloatingActionButton) findViewById(R.id.calibrate);
-        calibrateButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mDroneService.calibrate();
-            }
-        });
-        FloatingActionButton resetButton = (FloatingActionButton) findViewById(R.id.reset);
-        resetButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mDroneService.reset();
-            }
-        });
-        FloatingActionButton takeoffButton = (FloatingActionButton) findViewById(R.id.takeofflandfab);
-        takeoffButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mDroneService.takeoff();
-            }
-        });
+        ButterKnife.inject(this);
     }
 
+    public void onToggleClicked(View v) {
+        boolean on = ((ToggleButton) v).isChecked();
+        if(mDroneService != null) {
+            mDroneService.toggleUsingKalmanFilter();
+            mIsUsingKalman = !mIsUsingKalman;
+        }
+    }
+
+    @OnClick(R.id.mainfab)
+    public void mainFab(){
+        if (buttonPanel2.getVisibility() == View.GONE) {
+            buttonPanel2.setVisibility(View.VISIBLE);
+            buttonPanel.setVisibility(View.VISIBLE);
+            navDataPanel.setVisibility(View.GONE);
+        } else {
+            buttonPanel2.setVisibility(View.GONE);
+            buttonPanel.setVisibility(View.GONE);
+            navDataPanel.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @OnClick(R.id.stop)
+    public void stop(){
+        if (mDroneService != null) {
+            mDroneService.stop();
+        }
+    }
+
+    @OnClick(R.id.calibrate)
+    public void calibrate(){
+        if (mDroneService != null) {
+            mDroneService.calibrate();
+        }
+    }
+    @OnClick(R.id.reset)
+    public void reset(){
+        if (mDroneService != null) {
+            mDroneService.reset();
+        }
+    }
+    @OnClick(R.id.takeofflandfab)
+    public void takeoffland(){
+        if (mDroneService != null) {
+            mDroneService.takeoff();
+        }
+    }
+    @OnClick(R.id.forwardfab)
+    public void forward(){
+        if (mDroneService != null) {
+            mDroneService.forward();
+        }
+    }
+    @OnClick(R.id.backwardfab)
+    public void backward(){
+        if (mDroneService != null) {
+            mDroneService.backward();
+        }
+    }
+    @OnClick(R.id.leftfab)
+    public void left(){
+        if (mDroneService != null) {
+            mDroneService.left();
+        }
+    }
+    @OnClick(R.id.rightfab)
+    public void right(){
+        if (mDroneService != null) {
+            mDroneService.right();
+        }
+    }
+    @OnClick(R.id.upfab)
+    public void up(){
+        if (mDroneService != null) {
+            mDroneService.up();
+        }
+    }
+    @OnClick(R.id.downfab)
+    public void down(){
+        if (mDroneService != null) {
+            mDroneService.down();
+        }
+    }
+    @OnClick(R.id.clockwisefab)
+    public void clockwise(){
+        if (mDroneService != null) {
+            mDroneService.clockwise();
+        }
+    }
+    @OnClick(R.id.zerofab)
+    public void zero(){
+        if (mDroneService != null) {
+            mDroneService.zero();
+        }
+    }
+    @OnClick(R.id.followme)
+    public void followMe(){
+        if (mDroneService != null) {
+            mDroneService.followMe();
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -138,5 +209,75 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    //TODO: this method could use some cleanup. The final navdata that will be presented is still unknown so until I have narrowed down the data to what I want this method will stay the same.
+    @Override
+    public void updateNavData(final JSONObject navData) {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (navData.has("demo")) {
+                    JSONObject demo = navData.optJSONObject("demo");
+                    mFlyingState.setText(demo.optString("flyState"));
+                    mBatteryPercent.setText(demo.optString("batteryPercentage"));
+                    JSONObject rotation = demo.optJSONObject("rotation");
+                    mRotation.setText(
+                            " y= " + rotation.optString("y") +
+                                    "\nroll= " + rotation.optString("roll") +
+                                    "\nx= " + rotation.optString("x") +
+                                    "\nclockwise= " + rotation.optString("clockwise") +
+                                    "\nyaw= " + rotation.optString("yaw")
+                    );
+                    JSONObject velocity = demo.optJSONObject("velocity");
+                    mVelocity.setText(
+                            " x= " + velocity.optString("x") +
+                                    "\ny= " + velocity.optString("y") +
+                                    "\nz= " + velocity.optString("z")
+                    );
+                }
+                if (navData.has("magneto")) {
+                    JSONObject magneto = navData.optJSONObject("magneto");
+                    JSONObject heading = magneto.optJSONObject("heading");
+                    JSONObject offset = magneto.optJSONObject("offset");
+                    JSONObject rectified = magneto.optJSONObject("rectified");
 
+                    mMagneto.setText(
+                            " mx= " + magneto.optString("mx") +
+                                    "\nmy= " + magneto.optString("my") +
+                                    "\nmz= " + magneto.optString("mz") +
+                                    "\nradius= " + magneto.optString("radius") +
+                                    "\nheading= " + heading.optString("fusionUnwrapped") +
+                                    "\nrectifiedx= " + rectified.optString("x") +
+                                    "\nrectifiedy= " + rectified.optString("y") +
+                                    "\nrectifiedz= " + rectified.optString("z") +
+                                    "\noffestx= " + offset.optString("x") +
+                                    "\noffesty= " + offset.optString("y") +
+                                    "\noffestz= " + offset.optString("z")
+
+                    );
+                }
+                if (navData.has("gps")) {
+                    JSONObject gps = navData.optJSONObject("gps");
+                    mGPS.setText(
+                            " latitude= " + gps.optString("latitude") +
+                                    "\nlongitude= " + gps.optString("longitude") +
+                                    "\nelevation= " + gps.optString("elevation") +
+                                    "\ndataAvailable= " + gps.optString("dataAvailable") +
+                                    "\ndegree= " + gps.optString("degree")
+                    );
+                }
+            }
+        });
+    }
+
+    //This method updates the local position. It tells me where the drone Thinks I am in relation to it.
+    @Override
+    public void updateLocalPos(final JSONObject localprose) {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mXPos.setText(localprose.optString("x"));
+                mYPos.setText(localprose.optString("y"));
+            }
+        });
+    }
 }
