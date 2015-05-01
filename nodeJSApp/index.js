bfb329626fe18d58fad09068ef044f13c12fb72d
Aaron <amcintyre2@mail.csuchico.edu>
2015-05-01 17:16:38 +0930

    Autonomous flight working...kinda
    I have tested the autonomous flight using the lat lng from my phone provided over the socket connection.
    Still needs some tuning but in general it works.

--- nodeJSApp/index.js
+++ nodeJSApp/index.js
@@ -19,47 +19,45 @@ var controller = new autonomy.Controller(client, {debug: false});
 winston.add(winston.transports.File, { filename: 'dronebrain.log' });
 winston.remove(winston.transports.Console);
 var server = io.listen(port);
-
+var gotome = false;
 //callback for arclient
 var callback = function(err) { if (err) console.log(err); };
 
-var turncccallback = function(cb) {
-	controller.cw(180, cb);
-
-};
-
-var hovercallback = function() {
-	controller.hover();
-};
-
-var moveforwardcallback = function() {
-	controller.forward(0.5, callback);
-};
-
 //event handlers for the android app.
 server.sockets.on('connection', function(socket) {
+	
+	controller.on('localxy', function(data) {
+		socket.emit('localxy', data);
+	});
+	
 	client.on('navdata', function(navdata) {
 		if(!connected) {
 			connected = true;
 			console.log("droneconnected");
 		}
-		socket.emit('dronedata', navdata);
+	socket.emit('dronedata', navdata);
 	});
 	socket.on('location', function(data) {
 		winston.log('info', data.latitude);
 		winston.log('info', data.longitude);
-		console.log("long" + data.gps.longitude);
-		console.log("lat" + data.gps.latitude);	
+		console.log("long" + data.longitude);
+		console.log("lat" + data.latitude);	
+		var loc = {lat:0.0, lon:0.0};
+		loc.lat = data.latitude;
+		loc.lon = data.longitude;
+		if(gotome){
+			controller.gotolatlon(loc, callback);
+		}
 	});
 	
 	socket.on('takeoff', function(data) {
 		winston.log('info', data.message);
 		console.log("TAKEOFFDRONE");
 		if(flying == 0) {
-		//	client.takeoff(callback);
+			client.takeoff(callback);
 			flying = 1;	
 		} else {
-		//	client.land(callback);
+			client.land(callback);
 			flying = 0;
 		}
 	});
@@ -68,18 +66,60 @@ server.sockets.on('connection', function(socket) {
 		winston.log('info', data.message);
 		console.log("CALIBRATE");
 		//calibrate drone
-		//client.calibrate(0);
+		client.calibrate(0);
 	});
 
 	socket.on('reset', function(data) {
 		winston.log('info', data.message);
 		console.log("RESET");
+		client.disableEmergency();
+	});
+	socket.on('zero', function(data) {
+		console.log("ZERO");
+		controller.zero();
+		if(gotome)
+		{
+			gotome = false;
+		}
+		else
+		{
+			gotome = true;
+		}
 	});
 
-	socket.on('testdata', function(data) {
-		console.log("zero drone");
-		controller.zero();
-		console.log("lat " + controller.origin().lat + "lon " + controller.origin().lon);
+	socket.on('forward', function(data) {
+		console.log("FROWARD");
+		controller.forward(2, callback);
+
+	});
+	socket.on('backward', function(data) {
+		console.log("BACKWARD");
+		controller.backward(2, callback);
+
+	});
+	socket.on('left', function(data) {
+		console.log("LEFT");
+		controller.left(2, callback);
+
+	});
+	socket.on('right', function(data) {
+		console.log("RIGHT");
+		controller.right(2, callback);
+
+	});
+	socket.on('clockwise', function(data) {
+		console.log("CLOCKWISE");
+		controller.cw(45, callback);
+
+	});
+	socket.on('up', function(data) {
+		console.log("UP");
+		controller.up(1, callback);
+
+	});
+	socket.on('down', function(data) {
+		console.log("DOWN");
+		controller.down(1, callback);
 
 	});
 
